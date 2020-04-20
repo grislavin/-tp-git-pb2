f960aeb7053f3abf764e813e0685ab402c1137af
GrisLavin <grislavin@gmail.com>
2020-04-20 19:45:35 -0300

    calcu rama lavin

--- src/Calculadora/Calculadora.java
+++ src/Calculadora/Calculadora.java
@@ -8,7 +8,7 @@ public class Calculadora {
 		Integer b;
 		Integer opcion;
 		Integer resultado;
-//commit test - master if
+//commit test - master lavin
 		Scanner teclado = new Scanner(System.in);
 
 		do {
